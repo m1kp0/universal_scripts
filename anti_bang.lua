0affedc17acdc477ab0d1378f2ffc4b72545c9bb
m1kpe0 <yarikasik@inbox.ru>
2024-11-11 15:41:40 +0300

    Update anti_bang.lua

--- anti_bang.lua
+++ anti_bang.lua
@@ -1,17 +1,11 @@
 local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/m1kp0/libraries/refs/heads/main/m1kpe0_lime"))()
 local wind = lib:Window("anti bang")
 
-local localPlayer = game.Players.LocalPlayer
-local char = localPlayer.Character
-local localHMRP = char.HumanoidRootPart
-local CFrame = localHMRP.CFrame
-local pos = localHMRP.Position
-local oldPos = nil
-
 wind:Button("teleport", function()
-  oldPos = pos
+  oldPos = game.Players.LocalPlayer.Character.HumanoidRootPart.Position
   game.Workspace.FallenPartsDestroyHeight = -10000
-  CFrame = CFrame.new(0, -1000, 0)
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(0, -1000, 0)
   wait(1)
-  CFrame = CFrame.new(oldPos)
+  game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(oldPos)
+  game.Workspace.FallenPartsDestroyHeight = -100
 end)
