1f9193f9a19edb8a5410b63a3b344182d844991e
m1kpe0 <yarikasik@inbox.ru>
2025-04-05 17:21:32 +0300

    Update ONLY_PC_Trigger.lua

--- ONLY_PC_Trigger.lua
+++ ONLY_PC_Trigger.lua
@@ -1,22 +1,34 @@
-print("[SCRIPT]: loading..")
+print("starting")
 local others = game.Players
 local me = others.LocalPlayer
 local mouse = me:GetMouse()
 local get_others = game.Players:GetPlayers()
 local run_service = game:GetService("RunService")
 
-local auto_shoot = false
+-- toggles
 local last_click = 0
+local auto_shoot = false
+local team_check = false
+local died_check = false
 local max_distance = math.huge
 
+-- function 
 local function check_player()
     local hit = mouse.Target
     if hit then
         local character = hit:FindFirstAncestorOfClass("Model")
-        if character and others:GetPlayerFromCharacter(character) then
+        local player = others:GetPlayerFromCharacter(character)
+        if character and player then
             if auto_shoot and (character.HumanoidRootPart.Position - me.Character.HumanoidRootPart.Position).Magnitude < max_distance then
-                if character.Humanoid.Health > 0 then
-                    return true
+                if died_check and character.Humanoid.Health > 0 then 
+                    return true 
+                else 
+                    return true 
+                end
+                if team_check and me.TeamColor ~= player.TeamColor then 
+                    return true 
+                else 
+                    return true 
                 end
             end
         end
@@ -24,10 +36,13 @@ local function check_player()
     return false
 end
 
-print("[SCRIPT]: loaded functions. Loading gui..")
-local L = loadstring(Game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
-local W = L:NewWindow("Trigger")
-local Main = W:NewSection("Main")
+print("loading..")
+
+-- gui
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/wizard"))()
+local window = lib:NewWindow("Universal trigger")
+local Main = window:NewSection("Main")
+local Checkers = window:NewSection("Checkers")
 
 Main:CreateToggle("Toggle trigger", function(en)
     auto_shoot = en
@@ -36,15 +51,23 @@ Main:CreateToggle("Toggle trigger", function(en)
         if auto_shoot and check_player() then
             local current_click = tick()
             if current_click - last_click >= 0.01 then
-                mouse1click()
+                mouse1click() -- simulate click
                 last_click = current_click
             end
         end
     end)
 end)
 
-Main:CreateSlider("Max distance", 0, 1000, 1000, false, function(value)
-    max_distance = value
+Main:CreateSlider("Max distance", 1, 1000, 1000, false, function(value) 
+    max_distance = value 
 end)
 
-print("[SCRIPT]: loaded gui.")
+Checkers:CreateToggle("Died check", function(en) 
+    died_check = en 
+end)
+
+Checkers:CreateToggle("Team check", function(en) 
+    team_check = en 
+end)
+
+print("loaded")
