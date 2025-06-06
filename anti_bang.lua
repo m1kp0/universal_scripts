66d5100ac6ebb439e1edc98ae0e5247a552affc8
m1kpe0 <yarikasik@inbox.ru>
2025-06-06 20:50:54 +0300

    Update anti_bang.lua

--- anti_bang.lua
+++ anti_bang.lua
@@ -6,8 +6,7 @@ wind:Button("kill banger", function()
     local hrp = plr.Character.HumanoidRootPart
     local old_cframe = hrp.CFrame
     workspace.FallenPartsDestroyHeight = -10000
-    hrp.CFrame = CFrame.new(0, -1000, 0)
-    task.wait(1)
+    hrp.CFrame = CFrame.new(0, -1000, 0); task.wait(1)
     hrp.CFrame = old_cframe
     workspace.FallenPartsDestroyHeight = -100
 end)
